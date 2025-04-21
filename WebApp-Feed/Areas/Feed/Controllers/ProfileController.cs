4c2ffc7ffecb16e7466707d1d626507224f2d888
Aleksandr Kalinin <akalinin@sfedu.ru>
2025-04-21 14:55:51 +0300

    good post ui

--- WebApp-Feed/Areas/Feed/Controllers/ProfileController.cs
+++ WebApp-Feed/Areas/Feed/Controllers/ProfileController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApp_Feed.Database;
+using WebApp_Feed.Models;
 
 namespace WebApp_Feed.Areas.Feed.Controllers
 {
@@ -32,6 +34,36 @@ namespace WebApp_Feed.Areas.Feed.Controllers
             }
 
             return View(user);
+        }
+
+        // POST: //feed/postadd
+        [HttpPost]
+        [Route("profile/{username}")]
+        public async Task<IActionResult> Post(string username, [FromBody] string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+                return BadRequest("Текст поста не может быть пустым");
+
+            var user = _context.Users
+                .Include(u => u.Posts)
+                    .ThenInclude(p => p.Tags)
+                .Include(u => u.Posts)
+                    .ThenInclude(p => p.Interactions)
+                .Include(u => u.Interactions)
+                .FirstOrDefault(u => u.Username == username);
+
+            var post = new Post
+            {
+                Content = content,
+                CreatedAt = BitConverter.GetBytes(DateTime.UtcNow.ToBinary()),
+                PostType = "post",
+                UserId = user.UserId
+            };
+
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+
+            return Ok("Пост добавлен");
         }
 
     }
